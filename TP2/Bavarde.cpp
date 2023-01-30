e405c0144a7fd949b8f16aa67654702c76a8d157
chinawhwx <chinawhwx@gmail.com>
2023-01-30 17:57:24 +0100

    pas encore fini le Bavarde

---
+++ TP2/Bavarde.cpp
@@ -0,0 +1,26 @@
+#include "Bavarde.hpp"
+
+Bavarde::Bavarde()
+{
+}
+
+Bavarde::Bavarde(int)
+{
+}
+
+Bavarde::~Bavarde()
+{
+}
+
+void Bavarde::fonction(Bavarde b)
+{
+}
+
+int Bavarde::getNumero()
+{
+    return 0;
+}
+
+void Bavarde::afficher()
+{
+}
